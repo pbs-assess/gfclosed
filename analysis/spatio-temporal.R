8d81ad61cd9cd322d324682b0584cbfdb7a4a817
Elise Keppel <elise.keppel@dfo-mpo.gc.ca>
2020-05-02 18:57:40 -0700

    Clean up st model script

--- analysis/spatio-temporal.R
+++ analysis/spatio-temporal.R
@@ -1,9 +1,9 @@
-# density calcs, design-based
+# spatiotemporal model of biomass
 
 library(future)
 library(dplyr)
 plan(multisession, workers = availableCores() / 2)
-devtools::load_all()
+
 
 convert2utm <- function(df, coords = c("X", "Y"), out_crs = 3156) { # from yelloweye-inside utils
   x <- sf::st_as_sf(df,
@@ -32,9 +32,9 @@ my_function <- function(dat,
   ssid <- unique(dat$survey_series_id)
   survey <- unique(dat$survey_series_desc)
   species_name <- unique(dat$species_common_name) %>%
-    gsub(pattern = " ", replacement = "-")
+    gsub(pattern = " ", replacement = "_")
 
-# set up data
+  # set up data
   dat$density <- if (ssid %in% c(1, 3, 4, 16)) dat$density_kgpm2 else dat$density_ppkm2
   dat$density <- dat$density * 1000 # to keep numbers from being too small for optimizer
   dat <- dat %>%
@@ -46,108 +46,60 @@ my_function <- function(dat,
 
   dat <- mutate(dat, present = ifelse(density > 0, 1, 0))
   if (mean(dat$present) < 0.05) {
-    warning("Not enough data for SSID ", ssid, "; ", species_name, ".")
-    return(NULL)
+    message("Not enough data for SSID ", ssid, "; ", species_name, ".")
+  } else {
+
+    # prepare survey grid for each year
+    synoptic_grid <- gfplot::synoptic_grid %>%
+      mutate(survey_series_id = case_when(
+        survey == "SYN QCS" ~ 1,
+        survey == "SYN HS" ~ 3,
+        survey == "SYN WCVI" ~ 4,
+        survey == "SYN WCHG" ~ 16
+      ))
+    if (ssid %in% c(1, 3, 4, 16)) {
+      survey_grid <- synoptic_grid
+    }
+    if (ssid == 22) {
+      survey_grid <- gfplot::hbll_n_grid$grid
+      stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
+    }
+    if (ssid == 36) {
+      survey_grid <- gfplot::hbll_s_grid$grid
+      stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
+    }
+
+    survey_grid <- survey_grid %>%
+      dplyr::filter(survey_series_id == ssid) %>%
+      dplyr::select(.data$X, .data$Y) %>%
+      expand_prediction_grid(years = unique(dat$year))
+
+    # fit model
+    formula <- stats::as.formula(density ~ 0 + as.factor(year))
+    spde <- sdmTMB::make_spde(dat$X, dat$Y, n_knots = n_knots)
+
+    tictoc::tic(paste0("sdmTMB fit for ", species_name, "; SSID ", ssid))
+    m <- sdmTMB::sdmTMB(
+      formula = formula,
+      data = dat, time = "year", spde = spde, family = sdmTMB::tweedie(link = "log"),
+      anisotropy = anisotropy, silent = silent#,
+      #nlminb_loops = 2, newton_steps = 1
+    )
+    tictoc::toc()
+
+    predictions <- stats::predict(m, newdata = survey_grid, return_tmb_object = TRUE)
+    index <- sdmTMB::get_index(predictions, bias_correct = bias_correct)
+    index <- dplyr::mutate(index, cv = sqrt(exp(se^2) - 1))
+    list(
+      data = dat,
+      model = m,
+      spde = spde,
+      predictions = predictions,
+      index = index,
+      survey = survey,
+      species_name = species_name
+    )
   }
-
-# prepare survey grid for each year
-  synoptic_grid <- gfplot::synoptic_grid %>%
-    mutate(survey_series_id = case_when(
-      survey == "SYN QCS" ~ 1,
-      survey == "SYN HS" ~ 3,
-      survey == "SYN WCVI" ~ 4,
-      survey == "SYN WCHG" ~ 16
-    ))
-  if (ssid %in% c(1, 3, 4, 16)) {
-    survey_grid <- synoptic_grid
-  }
-  if (ssid == 22) {
-    survey_grid <- gfplot::hbll_n_grid$grid
-    stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
-  }
-  if (ssid == 36) {
-    survey_grid <- gfplot::hbll_s_grid$grid
-    stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
-  }
-
-  survey_grid <- survey_grid %>%
-    dplyr::filter(survey_series_id == ssid) %>%
-    dplyr::select(.data$X, .data$Y) %>%
-    expand_prediction_grid(years = unique(dat$year))
-
-  formula <- stats::as.formula(density ~ 0 + as.factor(year))
-  spde <- sdmTMB::make_spde(dat$X, dat$Y, n_knots = n_knots)
-
-  tictoc::tic(paste0("sdmTMB fit for ", species_name, "; SSID ", ssid))
-  m <- sdmTMB::sdmTMB(
-    formula = formula,
-    data = dat, time = "year", spde = spde, family = sdmTMB::tweedie(link = "log"),
-    anisotropy = anisotropy, silent = silent,
-    nlminb_loops = 2, newton_steps = 1
-  )
-  tictoc::toc()
-
-  predictions <- stats::predict(m, newdata = survey_grid, return_tmb_object = TRUE)
-  index <- sdmTMB::get_index(predictions, bias_correct = bias_correct)
-  index <- dplyr::mutate(index, cv = sqrt(exp(se^2) - 1))
-  list(
-    data = dat,
-    model = m,
-    spde = spde,
-    predictions = predictions,
-    index = index,
-    survey = survey,
-    species_name = species_name
-  )
-}
-
-# t <- my_function(dat = dat, species_rds = "D:/GitHub/pbs-assess/gfsynopsis-old/report/data-cache/yelloweye-rockfish.rds", ssid = 1)
-
-# path <- "D:/GitHub/pbs-assess/gfsynopsis-old/report/data-cache"
-# my_function(dat = dat, species_rds = file.path(path, species_name, ".rds"), ssid = ssid)
-
-if (Sys.info()[['user']] == "seananderson") {
-  dat <- readRDS("../gfsynopsis/report/data-cache/yelloweye-rockfish.rds")$survey_sets %>%
-    dplyr::filter(survey_series_id %in% c(1, 3))
-} else {
-  dat <- readRDS("D:/GitHub/pbs-assess/gfsynopsis-old/report/data-cache/yelloweye-rockfish.rds")$survey_sets %>%
-    dplyr::filter(survey_series_id %in% c(1, 3))
 }
 
 
-
-# d <- dat %>% group_split(survey_series_id) # works
-# test <- purrr::map(d, my_function)
-
-# test:
-sdmTMB_index <- function(dat){
-  dat %>% group_split(survey_series_id) %>%
-    purrr::map(my_function, silent = FALSE)
-}
-tictoc::tic()
-test_sdmTMB_index <- sdmTMB_index(dat)
-tictoc::toc()
-
-test_sdmTMB_index[[1]]$model
-test_sdmTMB_index[[1]]$model$sd_report
-test_sdmTMB_index[[1]]$predictions$data %>% head()
-test_sdmTMB_index[[1]]$index %>% head()
-
-test_sdmTMB_index[[2]]
-test_sdmTMB_index <- purrr::discard(test_sdmTMB_index, is.null)
-
-sdmTMB_index2 <- function(dat){
-  dat %>% group_split(survey_series_id) %>%
-    furrr::future_map(my_function)
-}
-tictoc::tic("Fitting all")
-test_sdmTMB_index2 <- sdmTMB_index2(dat)
-tictoc::toc()
-
-test_sdmTMB_index2 <- purrr::discard(test_sdmTMB_index2, is.null)
-
-spt_index <- purrr::map_dfr(test_sdmTMB_index2,
-  ~mutate(.$index, species_name = .$species_name, survey = .$survey)) %>%
-  as_tibble()
-
-spt_index
