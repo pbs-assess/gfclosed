1d729991afa7f06bb8fce38fb02b46c3d15ab728
Elise Keppel <elise.keppel@dfo-mpo.gc.ca>
2020-05-12 13:32:49 -0700

    Move st_biomass function

--- R/working_file.R
+++ R/working_file.R
@@ -89,12 +89,6 @@ plot_survey_pts <- function(data = survey_sets, ssid = NULL){
     guides(fill = guide_legend()) # TO DO: make legend
 }
 
-
-
-# ------------------------------------------------------------------------
-#
-# ------------------------------------------------------------------------
-
 #' Clip survey set data by proposed MPA closed areas
 #'
 #' @param data Survey set data for a species from `gfdata::get_survey_sets()`
@@ -112,31 +106,138 @@ clip_by_mpa <- function(data = survey_sets, ssid){
   return(data_exclude)
 }
 
+convert2utm <- function(df, coords = c("X", "Y"), out_crs = 3156) { # from yelloweye-inside utils
+  x <- sf::st_as_sf(df,
+    coords = coords, crs = 4326
+  ) %>%
+    sf::st_transform(crs = out_crs) %>%
+    sf::st_coordinates() %>%
+    as.data.frame()
+  x$X <- x$X / 1000
+  x$Y <- x$Y / 1000
+  dplyr::bind_cols(x,
+    df[, which(!names(df) %in% coords), drop = FALSE])
+}
+
+expand_prediction_grid <- function(grid, years) { # from yelloweye-inside utils
+  nd <- do.call("rbind",
+    replicate(length(years), grid, simplify = FALSE))
+  nd[["year"]] <- rep(years, each = nrow(grid))
+  nd
+}
+
+#' Calculate geostatistical survey biomass index
+#'
+#' @param dat Survey set data for one species and one survey series id.
+#' @param n_knots Number of knots for creating spde mesh.
+#' @param anisotropy Include TRUE/FALSE
+#' @param silent
+#' @param bias_correct
+#' @param species_name
+#'
+#' @return
+#' @export
+#'
+#' @examples
+st_biomass <- function(dat,
+  n_knots = 150, anisotropy = FALSE, silent = TRUE,
+  bias_correct = FALSE, species_name){
+
+  ssid <- unique(dat$survey_series_id)
+  survey <- unique(dat$survey_series_desc)
+  species_name <- unique(dat$species_common_name) %>%
+    gsub(pattern = " ", replacement = "_")
+
+  # set up data
+  dat$density <- if (ssid %in% c(1, 3, 4, 16)) dat$density_kgpm2 else dat$density_ppkm2
+  dat$density <- dat$density * 1000 # to keep numbers from being too small for optimizer
+  dat <- dat %>%
+    dplyr::filter(!(year == 2014 & survey_series_id == 16)) %>%
+    select(year, survey_series_id, fishing_event_id, longitude, latitude, density, species_code) %>%
+    mutate(Y = latitude, X = longitude)
+  dat <- dat[!duplicated(select(dat, year, fishing_event_id)), , drop = FALSE]
+  dat <- as_tibble(convert2utm(dat))
+
+  dat <- mutate(dat, present = ifelse(density > 0, 1, 0))
+  if (mean(dat$present) < 0.05) {
+    warning("Not enough data for SSID ", ssid, "; ", species_name, ".")
+    return(NULL)
+  }
+
+  # prepare survey grid for each year
+  synoptic_grid <- gfplot::synoptic_grid %>%
+    mutate(survey_series_id = case_when(
+      survey == "SYN QCS" ~ 1,
+      survey == "SYN HS" ~ 3,
+      survey == "SYN WCVI" ~ 4,
+      survey == "SYN WCHG" ~ 16
+    ))
+  if (ssid %in% c(1, 3, 4, 16)) {
+    survey_grid <- synoptic_grid
+  }
+  if (ssid == 22) {
+    survey_grid <- gfplot::hbll_n_grid$grid
+    stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
+  }
+  if (ssid == 36) {
+    survey_grid <- gfplot::hbll_s_grid$grid
+    stop("Non-synoptic surveys are not implemented yet.", call. = FALSE)
+  }
+
+  survey_grid <- survey_grid %>%
+    dplyr::filter(survey_series_id == ssid) %>%
+    dplyr::select(.data$X, .data$Y) %>%
+    expand_prediction_grid(years = unique(dat$year))
+
+  # fit model
+  formula <- stats::as.formula(density ~ 0 + as.factor(year))
+  spde <- sdmTMB::make_spde(dat$X, dat$Y, n_knots = n_knots)
+
+  tictoc::tic(paste0("sdmTMB fit for ", species_name, "; SSID ", ssid))
+  m <- sdmTMB::sdmTMB(
+    formula = formula,
+    data = dat, time = "year", spde = spde, family = sdmTMB::tweedie(link = "log"),
+    anisotropy = anisotropy, silent = silent#,
+    #nlminb_loops = 2, newton_steps = 1
+  )
+  tictoc::toc()
+
+  predictions <- stats::predict(m, newdata = survey_grid, return_tmb_object = TRUE)
+  index <- sdmTMB::get_index(predictions, bias_correct = bias_correct)
+  index <- dplyr::mutate(index, cv = sqrt(exp(se^2) - 1))
+  list(
+    data = dat,
+    model = m,
+    spde = spde,
+    predictions = predictions,
+    index = index,
+    survey = survey,
+    species_name = species_name
+  )
+}
+
+
 
 # ------------------------------------------------------------------------
 # Survey indices
 # ------------------------------------------------------------------------
 
-#' Survey design-based biomass index
-#'
-#' @param dat List containing survey set data by species (alread filtered for desired survey series id(s))
-#' @export
+
 design_biomass <- function(dat){
   dat %>% group_split(survey_series_id) %>%
-    # purrr::map(boot_biomass) # old way (retained here for trouble-shooting)
-    furrr::future_map_dfr(boot_biomass)
+    # purrr::map(boot_biomass) # retained here for trouble-shooting
+    furrr::future_map_dfr(boot_biomass) %>%
+    mutate(analysis = "design-based")
 }
 
-#' Survey st model-based biomass index
-#'
-#' @param dat List containing survey set data by species (alread filtered for desired survey series id(s))
-#' @export
+
 sdmTMB_biomass <- function(dat){
   dat %>% group_split(survey_series_id) %>%
-    # purrr::map(st_biomass)
+    # purrr::map(st_biomass) # retained here for trouble-shooting
     furrr::future_map(st_biomass) %>%
     purrr::discard(is.null) %>%
     purrr::map_dfr(~mutate(.$index, species_name = .$species_name, survey = .$survey)) %>%
-    as_tibble()
+    as_tibble() %>%
+    mutate(analysis = "geostatistical")
 }
 
